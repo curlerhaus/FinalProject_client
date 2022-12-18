875119fd69f703df693f010170adfb8708127d1e
curlerhaus <101598928+curlerhaus@users.noreply.github.com>
2022-12-18 12:19:11 -0600

    udpated heroku api locations.

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -2,7 +2,7 @@ import "./post.css";
 import { Link } from "react-router-dom";
 
 export default function Post({ post }) {
-  const PF = "http://localhost:3003/images/";
+  const PF = "https://sleepy-coast-84723.herokuapp.com/images/";
   return (
     <div className="post">
       {post.photo && (
